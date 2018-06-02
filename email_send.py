4b9ecb6c52e3f1432f092b5050fbb883ac9a19d1
vladosstrawberry <33176446+vladosstrawberry@users.noreply.github.com>
2018-06-02 20:57:37 +0300

    Update email_send.py

--- email_send.py
+++ email_send.py
@@ -5,7 +5,8 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
 from email.mime.application import MIMEApplication
 
-def make_email(me, passw, you, host, port, text=None, subject=None, html=None, images=None, image_names_in_html=None, file_to_add=None):
+def make_email(me, passw, you, host, port, text=None, subject=None, html=None,image=None, images=None, image_names_in_html=None,
+               file_to_add=None):
     msg = MIMEMultipart('alternative')
     if subject is not None:
         msg['subject'] = subject
@@ -15,19 +16,30 @@ def make_email(me, passw, you, host, port, text=None, subject=None, html=None, i
     msg['To'] = you
     if text is None and html is None:
         raise Exception("No text and html")
-    #data set up
+    # data set up
     if text is not None:
         part_text = MIMEText(text, 'plain')
         msg.attach(part_text)
     if html is not None:
         part_html = MIMEText(html, 'html')
-        #display images in html file
+        # display images in html file
+        if image is not None:
+            try:
+                fd =open(image, 'rb')
+                print(image)
+                print(basename(image))
+                part_image = MIMEImage(fd.read(), '{}'.format(splitext(basename(image))[0]))
+                part_image.add_header('Content-ID', '<{}>'.format(splitext(basename(image))[0]))
+                msg.attach(part_image)
+                fd.close()
+            except Exception:
+                print("Error occured")
         if images is not None and image_names_in_html is not None:
             for i, k in zip(images, image_names_in_html):
                 try:
                     fd = open(i, 'rb')
-                    part_image = MIMEImage(fd.read(), '{}'.format(  splitext(basename(i))[0]))
-                    part_image.add_header('Content-ID', '<{}>'.format(  splitext(basename(i))[0]))
+                    part_image = MIMEImage(fd.read(), '{}'.format(splitext(basename(i))[0]))
+                    part_image.add_header('Content-ID', '<{}>'.format(splitext(basename(i))[0]))
                     msg.attach(part_image)
                     fd.close()
                 except Exception:
@@ -37,14 +49,15 @@ def make_email(me, passw, you, host, port, text=None, subject=None, html=None, i
         for i in file_to_add:
             try:
                 fd = open(i, 'rb')
-                part = MIMEApplication(fd.read(), Name=''.format( splitext(basename(i))[0])) #gets only name
-                part['Content-Disposition'] = 'attachment; filename={}'.format(basename(i))  #gets only a file name with ext
+                part = MIMEApplication(fd.read(), Name=''.format(splitext(basename(i))[0]))  # gets only name
+                part['Content-Disposition'] = 'attachment; filename={}'.format(
+                    basename(i))  # gets only a file name with ext
                 msg.attach(part)
                 fd.close()
             except Exception:
                 print("Error")
 
-    #sending
+    # sending
 
     s = smtplib.SMTP_SSL(host, port)
     s.login(me, passw)
@@ -52,7 +65,6 @@ def make_email(me, passw, you, host, port, text=None, subject=None, html=None, i
     s.sendmail(me, you, text)
     s.quit()
 
-    
 
 if __name__ == '__main__':
     text = "Hi!\nHow are you?\nHere is the link you wanted:\nhttp://www.python.org"
